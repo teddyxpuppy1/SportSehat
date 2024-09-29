26f46ae83f44d541343f00c8c037e3e95a90cdac
teddyxpuppy1 <harikothapalli314@gmail.com>
2024-09-29 19:21:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -312,4 +312,4 @@ def submit_page():
     return render_template('submit.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
